533aea86d89169f3e61fc8110736de029a0061b8
Chester <chester.htooaunglinn@gmail.com>
2021-08-12 23:49:30 +0630

    feat: restructure project to simple one

--- core/lib/contants.ts
+++
@@ -1,2 +0,0 @@
-export const BLOCK_GENERATION_INTERVAL: number = 10;
-export const DIFFICULITY_ADJUSTMENT_INTERVAL: number = 10;
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import { getBlockchain } from '../src';
+import * as express from 'express';
+import { getBlockchain } from './blockchain';
 
 const initHttpServer = (port: number) => {
 	const app = express();
